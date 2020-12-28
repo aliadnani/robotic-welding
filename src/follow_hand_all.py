aec75dd4eabf585be6d9bb74dd155f4e053b0c49
aliadnani <ali.adnani@hotmail.com>
2020-12-28 19:06:58 +0800

    Feature: Follow hand position & rotation on Z-axis

--- src/follow_hand_all.py
+++ src/follow_hand_all.py
@@ -14,47 +14,17 @@ from scipy.spatial.transform import Rotation as R
 # I did not derive this nor do I fully understand the maths behind this :0
 # I took it from: https://dof.robotiq.com/discussion/1648/around-which-axes-are-the-rotation-vector-angles-defined
 def convert_tool_pose_to_transformation_matrix(tool_pose):
-    r = tool_pose[3:]
-    rx = r[0]
-    ry = r[1]
-    rz = r[2]
+    position_vector = np.array(tool_pose[:3]).reshape((3,1))
+    rotation_vector = np.array(tool_pose[3:])
+    
+    rotation_matrix = R.from_rotvec(rotation_vector).as_dcm()
 
-    theta = math.sqrt((rx ** 2) + (ry ** 2) + (rz ** 2))
+    transformation_matrix = np.append(rotation_matrix, position_vector, axis = 1)
+    transformation_matrix = np.append(transformation_matrix ,np.array([[0,0,0,1]]), axis=0)
+    return transformation_matrix
 
-    ux = rx / theta
-    uy = ry / theta
-    uz = rz / theta
-
-    c = math.cos(theta)
-    s = math.sin(theta)
-    C = 1 - c
-
-    base_to_tcp = tool_pose[:3]
-
-    T = np.array(
-        [
-            [
-                (ux * ux * C) + c,
-                (ux * uy * C) - (uz * s),
-                (ux * uz * C) + (uy * s),
-                base_to_tcp[0],
-            ],
-            [
-                (uy * ux * C) + (uz * s),
-                (uy * uy * C) + c,
-                (uy * uz * C) - (ux * s),
-                base_to_tcp[1],
-            ],
-            [
-                (uz * ux * C) - (uy * s),
-                (uz * uy * C) + (ux * s),
-                (uz * uz * C) + c,
-                base_to_tcp[2],
-            ],
-            [0, 0, 0, 1],
-        ]
-    )
-    return T
+def calculate_rotation_matrix(rotation_vector):
+    return R.from_rotvec(np.array(rotation_vector)).as_dcm()
 
 def calculate_coordinate_system_from_hand():
     pass
@@ -141,21 +111,98 @@ def main():
                         print('required_pose: %s' % (final_pose))
                         print('pose_difference: %s' % (pose_difference))
                         # Only moves robot if move is smaller than 8cm, minimizes robot moving in strange directions
-                        if pose_difference < 0.08:
-                            robot.movep(list(final_pose), acc=0.1, vel=0.2, wait=False)
+                        if pose_difference < 1:
+                            robot.movep(list(final_pose), acc=0.1, vel=0.1, wait=False)
 
         except:
             robot.close()
             sys.exit(0)
 
 
-def get_hand_basis(basis):
-    pass
+def get_hand_basis(frame):
+    basis_raw = frame.hands[0].basis
+    # X - positive to pinky
+    # Y - positive slap
+    # Z - positive thrust
+    basis = [list(basis_raw.x_basis.to_tuple()), list(basis_raw.y_basis.to_tuple()),  list(basis_raw.z_basis.to_tuple())]
+    return basis
 
 def test_transform():
+    # Leap motion 
+    controller = Leap.Controller()
+    controller.config.save()
     ### Orientation
     # Get hand basis matrix
-    # Calculate hand basis in absolute frame
+    robot = urx.Robot("192.168.0.2")
+    mytcp = m3d.Transform()  # create a matrix for our tool tcp
+    mytcp.pos.x = -0.0002
+    mytcp.pos.y = -0.144
+    mytcp.pos.z = 0.05
+    time.sleep(1)
+    robot.set_tcp(mytcp)
+    time.sleep(1)
+
+    while 1:
+        try:
+            tool_pose = get_tool_pose(robot)
+            T = convert_tool_pose_to_transformation_matrix(tool_pose)
+            # print(tool_pose[3:])
+            frame = controller.frame()
+            basis = get_hand_basis(frame)
+            if frame.hands[0].is_left:
+                print('LEFT')
+                for i in range(3):
+                    basis[0][i] = basis[0][i] * -1
+            # print(basis)
+            rotation_matrix = calculate_rotation_matrix(tool_pose[3:])
+            # basis_corrected = np.array(basis)
+            hand_basis_absolute = rotation_matrix.dot(np.array(basis).T)
+            # hand_basis_absolute = rotation_matrix.dot(np.array(basis))
+
+            # Basis is in a left-handed coordinate system,lets convert to right
+            hand_basis_absolute[0] = hand_basis_absolute[0] * np.array([-1, -1, 1])
+
+            orientation_matrix = np.array([[1,0,0],[0,1,0],[0,0,-1]])
+            final_basis = np.dot(hand_basis_absolute,orientation_matrix)
+            # print(final_basis)
+            final_basis_z_locked = []
+            for i in range(len(final_basis)):
+                row = final_basis[i]
+                if i == 2:
+                    row[0] = 0
+                    row[1] = 0
+                    row[2] = 1
+                else:
+                    row[2] = 0
+                final_basis_z_locked.append(row / np.linalg.norm(row))
+            final_basis_z_locked = np.array(final_basis_z_locked)
+
+            robot_orientation_matrix = np.array([[1,0,0],[0,1,0],[0,0,-1]])
+            wanted_basis = np.dot(final_basis,robot_orientation_matrix)
+
+
+            coordinate_system = np.array([[-1,0,0],[0,1,0],[0,0,1]])
+            inv_coordinate_system = np.linalg.inv(coordinate_system)
+
+            wanted_rotation_matrix = np.dot(inv_coordinate_system,wanted_basis)
+            wanted_rotation_vector = R.from_dcm(wanted_rotation_matrix).as_rotvec()
+
+            relative_palm_postion = read_hand_position(frame)
+            absolute_hand_position = calculate_hand_position(T, relative_palm_postion)
+            # print(final_basis_z_locked)
+            required_robot_position = (final_basis_z_locked[1] * 0.19) + np.array(absolute_hand_position)
+
+            final_pose = list(np.append(required_robot_position, wanted_rotation_vector))
+            print(final_pose)
+            print('--------------------------------------------------------\n')
+            robot.movep(final_pose, acc=0.1, vel=0.1, wait=False)
+            # time.sleep(0.2)
+            
+        except:
+            robot.close()
+            sys.exit(0)
+
+    # Calculate hand basis in absolute frame via rotation matrix
     # Lock hand basis to Z-axis
     # Flip x and y vectors of hand basis
     # Solve rotation matrix from coordiante frame to hand basis
@@ -173,9 +220,15 @@ def test_transform():
     
     # Transform vectors to absolute
     # right hand via c
+    # pass
+
+def test_math():
+    final_basis = np.array([[1,0,0], [0,1,0], [0,0,-1]])
+    rotation_matrix = R.from_rotvec(np.array([0,3.14,0])).as_dcm()
+    print( np.dot(  np.linalg.inv(rotation_matrix), final_basis))
     pass
 
-
 if __name__ == "__main__":
-    main()
-    # test_transform()
+    # main()
+    test_transform()
+    # test_math()
